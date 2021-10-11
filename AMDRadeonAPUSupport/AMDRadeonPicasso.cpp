5f610ccc91bf83d299886b7f54fe7df1f7e035af
VisualDevelopment <30368284+VisualDevelopment@users.noreply.github.com>
2021-10-11 16:00:53 +0300

    Add LICENSE

--- AMDRadeonAPUSupport/AMDRadeonPicasso.cpp
+++ AMDRadeonAPUSupport/AMDRadeonPicasso.cpp
@@ -1,19 +1,25 @@
 #include "AMDRadeonPicasso.hpp"
 
-OSDefineMetaClassAndStructors(AMDRadeonPicasso, super);
+OSDefineMetaClassAndStructors(AMDRadeonPicasso, IOService);
 
 void AMDRadeonPicasso::free() {
     super::free();
+
+    IOLog("AMDRadeonPicasso: free");
 }
 
 bool AMDRadeonPicasso::init(OSDictionary *properties) {
     if (!super::init(properties)) return false;
+
+    IOLog("AMDRadeonPicasso: init");
 
     return true;
 }
 
 bool AMDRadeonPicasso::start(IOService *provider) {
     if (!super::start(provider)) return false;
+
+    IOLog("AMDRadeonPicasso: start");
 
     return true;
 }
---
+++ README.md
@@ -0,0 +1,6 @@
+# AMDRadeonAPUSupport
+
+AMD APU graphics support kext
+
+The AMDRadeonAPUSupport kext is licensed under BSD-3.
+Source is based on mesa code.
