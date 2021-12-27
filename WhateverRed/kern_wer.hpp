a887cc3b5442e46c554a566861fee3d4e735bc0d
Visual <30368284+VisualDevelopment@users.noreply.github.com>
2021-12-27 13:36:53 +0200

    Add IOKit personalities and few patches for Picasso/Raven

--- WhateverRed/kern_wer.hpp
+++ WhateverRed/kern_wer.hpp
@@ -1,9 +1,15 @@
 #ifndef kern_wer_hpp
 #define kern_wer_hpp
 
+#include <Headers/kern_api.hpp>
+
 class WhateverRed {
 public:
     void init();
+
+private:
+    void processKernel(KernelPatcher &patcher);
+    void processKext(KernelPatcher &patcher, size_t index, mach_vm_address_t address, size_t size);
 };
 
 #endif /* kern_wer_hpp */
