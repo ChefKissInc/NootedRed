1939127a471e0dade9a6d9e4b289f57f5fe0dddc
Nyan Cat <17372086+e666666@users.noreply.github.com>
2023-01-24 17:02:27 +0800

    Fix compiler warning

--- WhateverRed/kern_wred.cpp
+++ WhateverRed/kern_wred.cpp
@@ -865,7 +865,7 @@ void *WRed::wrapAllocateAMDHWDisplay(void *that) {
 
 void WRed::wrapPspCosLog(void * pspData, uint32_t param2, uint64_t param3, uint32_t param4, uint64_t param5) {
     NETLOG("wred", "_psp_cos_log: pspData = %p param2 = 0x%X param3 = 0x%llX param4 = 0x%X param5 = 0x%llX", pspData, param2, param3, param4, param5);
-    NETLOG("wred", "Message: %s", param5);
+    NETLOG("wred", "Message: %s", (char*)param5);
     FunctionCast(wrapPspCosLog, callbackWRed->orgPspCosLog)(pspData, param2, param3, param4, param5);
     NETLOG("wred", "_psp_cos_log finished");
 }
