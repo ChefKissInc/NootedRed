6c7d46940844974cf51af64a439ef0ee0bca9885
Visual <30368284+ChefKissInc@users.noreply.github.com>
2023-02-04 20:13:58 +0200

    Undo PSP spoof
    Signed-off-by: Visual <30368284+ChefKissInc@users.noreply.github.com>

--- WhateverRed/kern_wred.cpp
+++ WhateverRed/kern_wred.cpp
@@ -487,11 +487,15 @@ uint32_t WRed::wrapSmuGetHwVersion(uint64_t param1, uint32_t param2) {
 uint32_t WRed::wrapPspSwInit(uint32_t *param1, uint32_t *param2) {
     switch (param1[3]) {
         case 0xA:
-            [[fallthrough]];
+            NETLOG("wred", "Spoofing PSP version v10 to v9.0.2");
+            param1[3] = 0x9;
+            param1[4] = 0x0;
+            param1[5] = 0x2;
+            break;
         case 0xB:
             [[fallthrough]];
         case 0xC:
-            NETLOG("wred", "Spoofing PSP version v10/v11/v12 to v11");
+            NETLOG("wred", "Spoofing PSP version v11/v12 to v11");
             param1[3] = 0xB;
             param1[4] = 0x0;
             param1[5] = 0x0;
