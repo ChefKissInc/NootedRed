4dd0e0486ac92f045c12c822f6b9a34b881350af
Visual <30368284+ChefKissInc@users.noreply.github.com>
2023-02-15 08:43:26 +0200

    Remove t_pspLoadExtended, GcFwConstant, SdmaFwConstant
    Signed-off-by: Visual <30368284+ChefKissInc@users.noreply.github.com>

--- WhateverRed/kern_amd.hpp
+++ WhateverRed/kern_amd.hpp
@@ -10,7 +10,6 @@ using t_Vega10PowerTuneConstructor = void (*)(void *that, void *param1, void *pa
 using t_HWEngineConstructor = void (*)(void *that);
 using t_HWEngineNew = void *(*)(size_t size);
 using t_sendMsgToSmc = uint32_t (*)(void *smumData, uint32_t msgId);
-using t_pspLoadExtended = uint32_t (*)(void *, uint64_t, uint64_t, const void *, size_t);
 
 constexpr uint32_t AMDGPU_FAMILY_RV = 0x8E;
 
@@ -51,18 +50,4 @@ struct CailInitAsicCapEntry {
     uint64_t revision, emulatedRev;
     uint64_t pciRev;
     void *caps, *goldenCaps;
-};
-
-struct GcFwConstant {
-    const char *unknown1;
-    uint32_t unknown2, size;
-    uint32_t addr, unknown4;
-    uint32_t unknown5, unknown6;
-    uint8_t *data;
-};
-
-struct SdmaFwConstant {
-    const char *unknown1;
-    uint32_t size, unknown2;
-    uint8_t *data;
 };
