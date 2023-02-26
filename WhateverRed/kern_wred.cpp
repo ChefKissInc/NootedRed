26df5bb2d04c614c5424fc14a38e0c8c99ec2a84
Visual <30368284+ChefKissInc@users.noreply.github.com>
2023-02-26 20:47:15 +0200

    Revert "Forgot to call memDesc->prepare"
    This reverts commit 7d8308219b521cfe8dd559fcae31867a02470cc5.

--- WhateverRed/kern_wred.cpp
+++ WhateverRed/kern_wred.cpp
@@ -45,10 +45,7 @@ void WRed::init() {
 
 void WRed::deinit() {
     if (this->vbiosData) { this->vbiosData->release(); }
-    if (this->memDesc) {
-        this->memDesc->complete();
-        this->memDesc->release();
-    }
+    if (this->memDesc) { this->memDesc->release(); }
 }
 
 void WRed::processPatcher(KernelPatcher &patcher) {
@@ -525,8 +522,6 @@ uint32_t WRed::wrapHwReadReg32(void *that, uint32_t reg) {
                        << 20;
         callbackWRed->memDesc = IOMemoryDescriptor::withAddress(reinterpret_cast<void *>(callbackWRed->fbOffset),
             memSize, kIODirectionNone);    // Mark iGPU's memory as reserved
-        PANIC_COND(!callbackWRed->memDesc, "wred", "Failed to reserve iGPU's VRAM");
-        callbackWRed->memDesc->prepare();
     }
     return FunctionCast(wrapHwReadReg32, callbackWRed->orgHwReadReg32)(that, reg == 0xD31 ? 0xD2F : reg);
 }
