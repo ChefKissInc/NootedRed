830f4983016d75030bc768301512203c13f8c824
Visual <30368284+ChefKissInc@users.noreply.github.com>
2023-04-06 08:22:12 +0300

    Update kern_model.hpp according to GPUOpen
    Signed-off-by: Visual <30368284+ChefKissInc@users.noreply.github.com>

--- NootedRed/kern_model.hpp
+++ NootedRed/kern_model.hpp
@@ -16,61 +16,17 @@ struct DevicePair {
     size_t modelNum;
 };
 
-static constexpr Model dev15D8[] = {
-    {0x00, "AMD Radeon Vega 8 WS"},
-    {0x91, "AMD Radeon Vega 3"},
-    {0x91, "AMD Radeon Vega Gfx"},
-    {0x92, "AMD Radeon Vega 3"},
-    {0x92, "AMD Radeon Vega Gfx"},
-    {0x93, "AMD Radeon Vega 1"},
-    {0xA1, "AMD Radeon Vega 10"},
-    {0xA2, "AMD Radeon Vega 8"},
-    {0xA3, "AMD Radeon Vega 6"},
-    {0xA4, "AMD Radeon Vega 3"},
-    {0xB1, "AMD Radeon Vega 10"},
-    {0xB2, "AMD Radeon Vega 8"},
-    {0xB3, "AMD Radeon Vega 6"},
-    {0xB4, "AMD Radeon Vega 3"},
-    {0xC1, "AMD Radeon Vega 10"},
-    {0xC2, "AMD Radeon Vega 8"},
-    {0xC3, "AMD Radeon Vega 6"},
-    {0xC4, "AMD Radeon Vega 3"},
-    {0xC5, "AMD Radeon Vega 3"},
-    {0xC8, "AMD Radeon Vega 11"},
-    {0xC9, "AMD Radeon Vega 8"},
-    {0xCA, "AMD Radeon Vega 11"},
-    {0xCB, "AMD Radeon Vega 8"},
-    {0xCC, "AMD Radeon Vega 3"},
-    {0xCE, "AMD Radeon Vega 3"},
-    {0xCF, "AMD Radeon Vega Gfx"},
-    {0xD1, "AMD Radeon Vega 10"},
-    {0xD2, "AMD Radeon Vega 8"},
-    {0xD3, "AMD Radeon Vega 6"},
-    {0xD4, "AMD Radeon Vega 3"},
-    {0xD8, "AMD Radeon Vega 11"},
-    {0xD9, "AMD Radeon Vega 8"},
-    {0xDA, "AMD Radeon Vega 11"},
-    {0xDB, "AMD Radeon Vega 3"},
-    {0xDB, "AMD Radeon Vega 8"},
-    {0xDC, "AMD Radeon Vega 3"},
-    {0xDD, "AMD Radeon Vega 3"},
-    {0xDE, "AMD Radeon Vega 3"},
-    {0xDF, "AMD Radeon Vega 3"},
-    {0xE3, "AMD Radeon Vega 3"},
-    {0xE4, "AMD Radeon Vega Gfx"},
-};
-
 static constexpr Model dev15DD[] = {
-    {0x81, "AMD Radeon Vega Gfx"},
-    {0x82, "AMD Radeon Vega Gfx"},
-    {0x83, "AMD Radeon Vega Gfx"},
+    {0x81, "AMD Radeon Vega 11"},
+    {0x82, "AMD Radeon Vega 8"},
+    {0x83, "AMD Radeon Vega 8"},
     {0x84, "AMD Radeon Vega 6"},
-    {0x85, "AMD Radeon Vega Gfx"},
+    {0x85, "AMD Radeon Vega 3"},
     {0x86, "AMD Radeon Vega 11"},
     {0x88, "AMD Radeon Vega 8"},
     {0xC1, "AMD Radeon Vega 11"},
     {0xC2, "AMD Radeon Vega 8"},
-    {0xC3, "AMD Radeon Vega 3/10"},
+    {0xC3, "AMD Radeon Vega 10"},
     {0xC4, "AMD Radeon Vega 8"},
     {0xC5, "AMD Radeon Vega 3"},
     {0xC6, "AMD Radeon Vega 11"},
@@ -93,9 +49,52 @@ static constexpr Model dev15DD[] = {
     {0xE2, "AMD Radeon Vega 3"},
 };
 
+static constexpr Model dev15D8[] = {
+    {0x00, "AMD Radeon Vega 8 WS"},
+    {0x91, "AMD Radeon Vega 3"},
+    {0x92, "AMD Radeon Vega 3"},
+    {0x93, "AMD Radeon Vega 1"},
+    {0xA1, "AMD Radeon Vega 10"},
+    {0xA2, "AMD Radeon Vega 8"},
+    {0xA3, "AMD Radeon Vega 6"},
+    {0xA4, "AMD Radeon Vega 3"},
+    {0xB1, "AMD Radeon Vega 10"},
+    {0xB2, "AMD Radeon Vega 8"},
+    {0xB3, "AMD Radeon Vega 6"},
+    {0xB4, "AMD Radeon Vega 3"},
+    {0xC1, "AMD Radeon Vega 10"},
+    {0xC2, "AMD Radeon Vega 8"},
+    {0xC3, "AMD Radeon Vega 6"},
+    {0xC4, "AMD Radeon Vega 3"},
+    {0xC5, "AMD Radeon Vega 3"},
+    {0xC8, "AMD Radeon Vega 11"},
+    {0xC9, "AMD Radeon Vega 8"},
+    {0xCA, "AMD Radeon Vega 11"},
+    {0xCB, "AMD Radeon Vega 8"},
+    {0xCC, "AMD Radeon Vega 3"},
+    {0xCE, "AMD Radeon Vega 3"},
+    {0xCF, "AMD Radeon Vega 3"},
+    {0xD1, "AMD Radeon Vega 10"},
+    {0xD2, "AMD Radeon Vega 8"},
+    {0xD3, "AMD Radeon Vega 6"},
+    {0xD4, "AMD Radeon Vega 3"},
+    {0xD8, "AMD Radeon Vega 11"},
+    {0xD9, "AMD Radeon Vega 8"},
+    {0xDA, "AMD Radeon Vega 11"},
+    {0xDB, "AMD Radeon Vega 3"},
+    {0xDC, "AMD Radeon Vega 3"},
+    {0xDD, "AMD Radeon Vega 3"},
+    {0xDE, "AMD Radeon Vega 3"},
+    {0xDF, "AMD Radeon Vega 3"},
+    {0xE1, "AMD Radeon Vega 11"},
+    {0xE2, "AMD Radeon Vega 9"},
+    {0xE3, "AMD Radeon Vega 3"},
+    {0xE4, "AMD Radeon Vega 3"},
+};
+
 static constexpr DevicePair devices[] = {
-    {0x15D8, dev15D8, arrsize(dev15D8)},
     {0x15DD, dev15DD, arrsize(dev15DD)},
+    {0x15D8, dev15D8, arrsize(dev15D8)},
 };
 
 inline const char *getBranding(uint16_t dev, uint16_t rev) {
@@ -109,22 +108,7 @@ inline const char *getBranding(uint16_t dev, uint16_t rev) {
         }
     }
 
-    switch (dev) {
-        case 0x15D8:
-            return "AMD Radeon Vega (Picasso)";
-        case 0x15DD:
-            return "AMD Radeon Vega (Raven Ridge)";
-        case 0x164C:
-            return "AMD Radeon Vega (Lucienne)";
-        case 0x1636:
-            return "AMD Radeon Vega (Renoir)";
-        case 0x15E7:
-            return "AMD Radeon Vega (Barcelo)";
-        case 0x1638:
-            return "AMD Radeon Vega (Cezanne)";
-        default:
-            return nullptr;
-    }
+    return "AMD Radeon Graphics";
 }
 
 #endif /* kern_model_hpp */
