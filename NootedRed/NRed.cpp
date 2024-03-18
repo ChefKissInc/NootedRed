60324372d98951382d364edfaa4371316a6c6908
Visual Ehrmanntraut <30368284+VisualEhrmanntraut@users.noreply.github.com>
2024-03-18 19:34:06 +0200

    Update all the copyright dates
    Signed-off-by: Visual Ehrmanntraut <30368284+VisualEhrmanntraut@users.noreply.github.com>

--- NootedRed/NRed.cpp
+++ NootedRed/NRed.cpp
@@ -1,4 +1,4 @@
-//! Copyright © 2022-2023 ChefKiss Inc. Licensed under the Thou Shalt Not Profit License version 1.5.
+//! Copyright © 2022-2024 ChefKiss Inc. Licensed under the Thou Shalt Not Profit License version 1.5.
 //! See LICENSE for details.
 
 #include "NRed.hpp"
