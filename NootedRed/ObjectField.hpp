34ad6b95d1f00ccfa53eff0402c2a1c595dcaf18
Visual Ehrmanntraut <30368284+VisualEhrmanntraut@users.noreply.github.com>
2024-08-24 10:47:14 +0300

    refactor: Make ObjectField methods inline

--- NootedRed/ObjectField.hpp
+++ NootedRed/ObjectField.hpp
@@ -8,19 +8,19 @@ class ObjectField {
     UInt32 offset {InvalidOffset};
 
     public:
-    void operator=(const UInt32 offset) {
+    inline void operator=(const UInt32 offset) {
         PANIC_COND(offset == InvalidOffset, "ObjField", "offset == InvalidOffset");
         this->offset = offset;
     }
 
-    ObjectField<T> operator+(const UInt32 value) {
+    inline ObjectField<T> operator+(const UInt32 value) {
         PANIC_COND(this->offset == InvalidOffset, "ObjField", "value == InvalidOffset");
         ObjectField<T> ret {};
         ret.offset = this->offset + value;
         return ret;
     }
 
-    T &get(void *that) {
+    inline T &get(void *that) {
         PANIC_COND(that == nullptr, "ObjField", "that == nullptr");
         PANIC_COND(this->offset == InvalidOffset, "ObjField", "this->offset == InvalidOffset");
         return getMember<T>(that, this->offset);
