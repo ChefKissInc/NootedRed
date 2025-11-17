c657eb73300b2d19a3bd3734465ec01ccc93d68d
Visual Ehrmanntraut <30368284+VisualEhrmanntraut@users.noreply.github.com>
2025-11-17 11:24:23 +0200

    refactor: format files

--- NootedRed/GPUDriversAMD/CAIL/Result.hpp
+++ NootedRed/GPUDriversAMD/CAIL/Result.hpp
@@ -21,23 +21,23 @@ enum CAILResult {
 
 inline CAILResult processSMUFWResponse(const UInt32 value) {
     switch (value) {
-        case kSMUFWResponseNoResponse: 
+        case kSMUFWResponseNoResponse:
             return kCAILResultNoResponse;
-        case kSMUFWResponseSuccess: 
+        case kSMUFWResponseSuccess:
             return kCAILResultOK;
-        case kSMUFWResponseRejectedBusy: 
+        case kSMUFWResponseRejectedBusy:
             SYSTRACE("CAIL", "SMU FW command rejected; SMU is busy");
             return kCAILResultInvalidParameters;
-        case kSMUFWResponseRejectedPrereq: 
+        case kSMUFWResponseRejectedPrereq:
             SYSTRACE("CAIL", "SMU FW command rejected; prerequisite was not satisfied");
             return kCAILResultInvalidParameters;
-        case kSMUFWResponseUnknownCommand: 
+        case kSMUFWResponseUnknownCommand:
             SYSTRACE("CAIL", "Unknown SMU FW command");
             return kCAILResultUnsupported;
-        case kSMUFWResponseFailed: 
+        case kSMUFWResponseFailed:
             SYSTRACE("CAIL", "SMU FW command failed");
             return kCAILResultInvalidParameters;
-        default: 
+        default:
             SYSTRACE("CAIL", "Unknown SMU FW response %d", value);
             return kCAILResultInvalidParameters;
     }
