0e41eec14fed48805626d7e78fd58bffc4a94657
Visual Ehrmanntraut <30368284+VisualEhrmanntraut@users.noreply.github.com>
2026-05-15 00:07:30 +0300

    iVega/AppleGFXHDA: Rewrite to not use lookup patch
    Also needed for #421

--- NootedRed/iVega/AppleGFXHDA.cpp
+++ NootedRed/iVega/AppleGFXHDA.cpp
@@ -3,40 +3,27 @@
 // Copyright © 2022-2025 ChefKiss. Licensed under the Thou Shalt Not Profit License version 1.5.
 // See LICENSE for details.
 
+#include <Headers/kern_iokit.hpp>
 #include <Headers/kern_patcher.hpp>
 #include <Headers/kern_util.hpp>
 #include <Kexts.hpp>
 #include <NRed.hpp>
 #include <iVega/AppleGFXHDA.hpp>
 #include <libkern/OSTypes.h>
-#include <mach/i386/vm_types.h>
 
-constexpr UInt32 AMDVendorID        = 0x1002;
-constexpr UInt32 Navi10HDMIDeviceID = 0xAB38;
-constexpr UInt32 Navi10HDMIID       = (Navi10HDMIDeviceID << 16) | AMDVendorID;
-constexpr UInt32 RavenHDMIDeviceID  = 0x15DE;
-constexpr UInt32 RavenHDMIID        = (RavenHDMIDeviceID << 16) | AMDVendorID;
-constexpr UInt32 RenoirHDMIDeviceID = 0x1637;
-constexpr UInt32 RenoirHDMIID       = (RenoirHDMIDeviceID << 16) | AMDVendorID;
+static constexpr UInt32 AMDVendorID        = 0x1002;
+static constexpr UInt32 RavenHDMIDeviceID  = 0x15DE;
+static constexpr UInt32 RenoirHDMIDeviceID = 0x1637;
 
-static iVega::AppleGFXHDA instance;
+static iVega::AppleGFXHDA moduleInstance;
 
-iVega::AppleGFXHDA& iVega::AppleGFXHDA::singleton() { return instance; }
+iVega::AppleGFXHDA& iVega::AppleGFXHDA::singleton() { return moduleInstance; }
 
 void iVega::AppleGFXHDA::processKext(KernelPatcher& patcher, size_t id, mach_vm_address_t slide, size_t size)
 {
     if (kextAppleGFXHDA.loadIndex != id) { return; }
 
     NRed::singleton().hwLateInit();
-
-    const UInt32 probeFind = Navi10HDMIID;
-    const UInt32 probeRepl = NRed::singleton().getAttributes().isRenoir() ? RenoirHDMIID : RavenHDMIID;
-    const KernelPatcher::LookupPatch patch{&kextAppleGFXHDA, reinterpret_cast<const UInt8*>(&probeFind),
-                                           reinterpret_cast<const UInt8*>(&probeRepl), sizeof(probeFind), 1};
-    patcher.clearError();
-    patcher.applyLookupPatch(&patch);
-    PANIC_COND(patcher.getError() != KernelPatcher::Error::NoError, "AGFXHDA",
-               "Failed to apply patch for HDMI controller probe");
 
     KernelPatcher::SolveRequest solveRequests[] = {
         {"__ZN34AppleGFXHDAFunctionGroupATI_Tahiti10gMetaClassE", this->orgFunctionGroupTahiti},
@@ -45,18 +32,61 @@ void iVega::AppleGFXHDA::processKext(KernelPatcher& patcher, size_t id, mach_vm_
     PANIC_COND(!patcher.solveMultiple(id, solveRequests, slide, size, true), "AGFXHDA", "Failed to solve symbols");
 
     KernelPatcher::RouteRequest requests[] = {
+        {                                  "__ZN21AppleGFXHDAController5probeEP9IOServicePi",wrapProbe,this->orgProbe                                         },
         {"__ZN31AppleGFXHDAFunctionGroupFactory27createAppleHDAFunctionGroupEP11DevIdStruct",
-         wrapCreateAppleHDAFunctionGroup,this->orgCreateAppleHDAFunctionGroup },
+         wrapCreateAppleHDAFunctionGroup, this->orgCreateAppleHDAFunctionGroup},
         {              "__ZN24AppleGFXHDAWidgetFactory20createAppleHDAWidgetEP11DevIdStruct", wrapCreateAppleHDAWidget,
-         this->orgCreateAppleHDAWidget},
+         this->orgCreateAppleHDAWidget                                        },
     };
     PANIC_COND(!patcher.routeMultiple(id, requests, slide, size), "AGFXHDA", "Failed to route symbols");
 }
 
+IOService* iVega::AppleGFXHDA::wrapProbe(IOService* that, IOService* provider, SInt32* score)
+{
+    const auto dev = OSDynamicCast(IOPCIDevice, provider);
+    if (dev == nullptr) { return FunctionCast(wrapProbe, singleton().orgProbe)(that, provider, score); }
+
+    const auto vendorID = WIOKit::readPCIConfigValue(dev, WIOKit::kIOPCIConfigVendorID);
+    const auto deviceID = WIOKit::readPCIConfigValue(dev, WIOKit::kIOPCIConfigDeviceID);
+    if (vendorID != AMDVendorID || (deviceID != RavenHDMIDeviceID && deviceID != RenoirHDMIDeviceID)) {
+        DBGLOG("GFXHDA", "Device is not Raven or Renoir, calling org.");
+        return FunctionCast(wrapProbe, singleton().orgProbe)(that, provider, score);
+    }
+
+    WIOKit::renameDevice(dev, "HDAU");
+
+    UInt8 bytes[] = {0x00};
+    dev->setProperty("built-in", bytes, sizeof(bytes));
+
+    that->setProperty("IsNRedHDA", bytes, sizeof(bytes));
+    dev->setProperty("IsNRedHDA", bytes, sizeof(bytes));
+
+    singleton().controller.provider(that)                   = provider;
+    singleton().controller.vendorID(that)                   = vendorID;
+    singleton().controller.deviceID(that)                   = deviceID;
+    singleton().controller.integratedCodecAddressMask(that) = 0x1;
+    singleton().controller.codecAddressMask(that)           = 0x1;
+    singleton().controller.useRirb(that)                    = true;
+    singleton().controller.intIndex(that)                   = 1;
+    singleton().controller.inputSampleLatency(that)         = 1;
+    singleton().controller.outputSampleLatency(that)        = 1;
+    singleton().controller.inputSafetyOffset(that)          = 1;
+    singleton().controller.outputSafetyOffset(that)         = 1;
+    singleton().controller.inputSafetyOffsetLowPower(that)  = 1;
+    singleton().controller.outputSafetyOffsetLowPower(that) = 1;
+    singleton().controller.inputEntrySize(that)             = 0x3000;
+    singleton().controller.outputEntrySize(that)            = 0x3000;
+    singleton().controller.regAccessReady(that)             = true;
+
+    DBGLOG("GFXHDA", "Initialised Raven or Renoir device.");
+
+    return that;
+}
+
 void* iVega::AppleGFXHDA::wrapCreateAppleHDAFunctionGroup(void* devId)
 {
-    auto vendorID = getMember<UInt16>(devId, 0x2);
-    auto deviceID = getMember<UInt32>(devId, 0x8);
+    const auto vendorID = getMember<UInt16>(devId, 0x2);
+    const auto deviceID = getMember<UInt32>(devId, 0x8);
     if (vendorID == AMDVendorID && (deviceID == RavenHDMIDeviceID || deviceID == RenoirHDMIDeviceID)) {
         return singleton().orgFunctionGroupTahiti->alloc();
     }
--- NootedRed/iVega/AppleGFXHDA.hpp
+++ NootedRed/iVega/AppleGFXHDA.hpp
@@ -6,14 +6,78 @@
 #pragma once
 #include <Headers/kern_patcher.hpp>
 #include <Headers/kern_util.hpp>
+#include <IOKit/IOService.h>
+#include <PenguinWizardry/KernelVersion.hpp>
+#include <PenguinWizardry/ObjectField.hpp>
 
 namespace iVega
 {
 
+    struct GFXHDAController
+    {
+        ObjectField<IOService*> provider;
+        ObjectField<uint32_t>   vendorID;
+        ObjectField<uint32_t>   deviceID;
+        ObjectField<uint16_t>   integratedCodecAddressMask;
+        ObjectField<uint16_t>   codecAddressMask;
+        ObjectField<bool>       useRirb;
+        ObjectField<uint8_t>    intIndex;
+        ObjectField<uint32_t>   inputSampleLatency;
+        ObjectField<uint32_t>   outputSampleLatency;
+        ObjectField<uint32_t>   inputSafetyOffset;
+        ObjectField<uint32_t>   outputSafetyOffset;
+        ObjectField<uint32_t>   inputSafetyOffsetLowPower;
+        ObjectField<uint32_t>   outputSafetyOffsetLowPower;
+        ObjectField<uint32_t>   inputEntrySize;
+        ObjectField<uint32_t>   outputEntrySize;
+        ObjectField<bool>       regAccessReady;
+
+        GFXHDAController()
+        {
+            this->provider = 0x98;
+            if (currentKernelVersion() >= MACOS_10_15) {
+                this->vendorID                   = 0xEF0;
+                this->deviceID                   = 0xEF4;
+                this->integratedCodecAddressMask = 0x2CA;
+                this->codecAddressMask           = 0x2CC;
+                this->useRirb                    = 0x414;
+                this->intIndex                   = 0xE69;
+                this->inputSampleLatency         = 0xE6C;
+                this->outputSampleLatency        = 0xE70;
+                this->inputSafetyOffset          = 0xE74;
+                this->outputSafetyOffset         = 0xE78;
+                this->inputSafetyOffsetLowPower  = 0xE7C;
+                this->outputSafetyOffsetLowPower = 0xE80;
+                this->inputEntrySize             = 0x420;
+                this->outputEntrySize            = 0x424;
+                this->regAccessReady             = 0xEDC;
+            }
+            else {
+                this->vendorID                   = 0xED8;
+                this->deviceID                   = 0xEDC;
+                this->integratedCodecAddressMask = 0x2BA;
+                this->codecAddressMask           = 0x2BC;
+                this->useRirb                    = 0x404;
+                this->intIndex                   = 0xE59;
+                this->inputSampleLatency         = 0xE5C;
+                this->outputSampleLatency        = 0xE60;
+                this->inputSafetyOffset          = 0xE64;
+                this->outputSafetyOffset         = 0xE68;
+                this->inputSafetyOffsetLowPower  = 0xE6C;
+                this->outputSafetyOffsetLowPower = 0xE70;
+                this->inputEntrySize             = 0x410;
+                this->outputEntrySize            = 0x414;
+                this->regAccessReady             = 0xEC4;
+            }
+        }
+    };
+
     class AppleGFXHDA
     {
+        GFXHDAController  controller;
         OSMetaClass*      orgFunctionGroupTahiti{nullptr};
         OSMetaClass*      orgWidget1002AAA0{nullptr};
+        mach_vm_address_t orgProbe{0};
         mach_vm_address_t orgCreateAppleHDAFunctionGroup{0};
         mach_vm_address_t orgCreateAppleHDAWidget{0};
 
@@ -23,8 +87,9 @@ namespace iVega
         void processKext(KernelPatcher& patcher, size_t id, mach_vm_address_t slide, size_t size);
 
     private:
-        static void* wrapCreateAppleHDAFunctionGroup(void* devId);
-        static void* wrapCreateAppleHDAWidget(void* devId);
+        static IOService* wrapProbe(IOService* that, IOService* provider, SInt32* score);
+        static void*      wrapCreateAppleHDAFunctionGroup(void* devId);
+        static void*      wrapCreateAppleHDAWidget(void* devId);
     };
 
 };    // namespace iVega
