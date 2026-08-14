56d63e9bf1fe2a164fde1281fbb462d188cd8f2d
Visual <30368284+VisualEhrmanntraut@users.noreply.github.com>
2026-08-15 01:57:11 +0300

    GPUDriversAMD/RavenIPOffset: Remove now-unused SDMA0 constant

--- NootedRed/GPUDriversAMD/RavenIPOffset.hpp
+++ NootedRed/GPUDriversAMD/RavenIPOffset.hpp
@@ -8,7 +8,6 @@
 
 constexpr UInt32 NBIO_BASE_2  = 0xD20;
 constexpr UInt32 GC_BASE_0    = 0x2000;
-constexpr UInt32 SDMA0_BASE_0 = 0x1260;
 constexpr UInt32 DCN_BASE_2   = 0x34C0;
 constexpr UInt32 MP0_BASE_0   = 0x16000;
 constexpr UInt32 SMUIO_BASE_0 = 0x16800;
